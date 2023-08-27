51fcabf4721fc70ff727e7f4b1474df6e83e3278
Prabhat Kumar <2012prabhatgss@gmail.com>
2023-08-27 15:52:02 +0530

    new update

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -26,7 +26,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
               name: `${tour.name} Tour`,
               description: tour.summary,
               images: [
-                `https://www.natours.dev/img/tours/${tour.imageCover}`
+                `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
               ],
             },
           },
